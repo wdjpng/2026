16e84b83ace8e479203bc9749456822533955c84
Bili-Sakura <Bili-Sakura@users.noreply.github.com>
2025-12-02 13:39:59 +0000

    Deploying to gh-pages from @ iclr-blogposts/2026@691b4a2db55590bf1f8d69dc9fd4fa896f06d2d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -160,6 +160,17 @@ ninja.data = [{
             window.location.href = "/2026/blog/2026/speeding-up-rl/";
           
         },
+      },{id: "post-from-remote-sensing-foundation-models-to-world-models-a-pathway-in-the-perspective-of-generative-modeling",
+        
+          title: "From Remote Sensing Foundation Models to World Models: A pathway in the perspective...",
+        
+        description: "This blog post explores the pathway from remote sensing foundation models to world models through the lens of Generative Modeling, arguing that true understanding of the physical world comes from the ability to recreate it.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/2026/blog/2026/remote-sensing-foundation-models-to-world-models/";
+          
+        },
       },{id: "post-pushing-meta-continual-learning-algorithms-to-the-limit",
         
           title: "Pushing Meta-Continual Learning Algorithms to the Limit",
